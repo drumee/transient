6e960cf2fa856b70ab429424a260915bedf3f7c8
Somanos Sar <somanos@drumee.com>
2026-02-20 18:58:14 +0100

    Fixed media wrong destination in move

--- service/private/media.js
+++ service/private/media.js
@@ -304,6 +304,9 @@ class __private_media extends Media {
           break;
         case "show":
           let nid = node.nid;
+          if (!dest) {
+            dest = this.heap.dest
+          }
           let access = `${node.des_db}.mfs_access_node`;
           let sockets = await this.yp.await_proc("entity_sockets", {
             db_name: node.des_db,
@@ -325,21 +328,21 @@ class __private_media extends Media {
               nid = r.actual_home_id;
             }
             delete r.args;
-            const dest = { ...r };
+            // const dest = { ...r };
             const src = { ...this.heap.oldItems[s.uid] }
             r.args = { tag, src, dest, changelog: this.__changelog };
-            let c = null;
-            if (counts[s.uid]) {
-              c = counts[s.uid];
-            } else {
-              let proc = `${r.actual_db}.mfs_count_new`;
-              c = await this.yp.await_proc(proc, nid, s.uid);
-              counts[s.uid] = c;
-            }
+            // let c = null;
+            // if (counts[s.uid]) {
+            //   c = counts[s.uid];
+            // } else {
+            //   let proc = `${r.actual_db}.mfs_count_new`;
+            //   c = await this.yp.await_proc(proc, nid, s.uid);
+            //   counts[s.uid] = c;
+            // }
 
-            r.new_chat = c.new_chat;
-            r.new_file = c.new_file;
-            r.hubs = c.hubs;
+            // r.new_chat = c.new_chat;
+            // r.new_file = c.new_file;
+            // r.hubs = c.hubs;
             nodes[s.uid] = r;
             await RedisStore.sendData(this.payload(r), s);
           }
@@ -870,7 +873,6 @@ class __private_media extends Media {
         denied.push(source_node);
       }
     }
-    this.debug("AAA:85", denied, this.heap.srcgrantlst, src, dest)
     if (!isEmpty(denied)) {
       this.warn("Got denied nodes", denied)
       this._done();
@@ -925,7 +927,6 @@ class __private_media extends Media {
         case "showone":
           if (!row.dest_db_name) {
             let entity = await this.yp.await_proc('get_entity', this.input.get('recipient_id'))
-            // this.warn("AAA:448 -- GOT NULL DEST DB. Using default", entity, row);
             proc = `${entity.db_name}.mfs_access_node`;
             r = await this.db.await_proc(proc, this.uid, row.nid);
             r.privilege = r.permission;
@@ -1217,7 +1218,6 @@ class __private_media extends Media {
       this.uid,
       Permission.MODIFY
     );
-    this.debug("AAA:pre_trash:mfs_chk_pre_trash", data)
     if (!isEmpty(data)) {
       this.exception.user("_delete_hub");
       return;
@@ -1263,8 +1263,6 @@ class __private_media extends Media {
       this.uid,
       Permission.MODIFY
     );
-    this.debug(`AAA:trash:this.heap.nodes`, this.heap.nodes)
-    this.debug(`AAA:trash:mfs_pre_trash_next dbname=${this.dbname}`, this.uid, granted, Permission.MODIFY, data)
     let keys = [Attr.nid, Attr.hub_id];
     let service = "media.remove";
     let recipients;
