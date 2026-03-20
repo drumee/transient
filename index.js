fff2316e6833b71076404e0f137b906d390d3b85
Somanos Sar <somanos@drumee.com>
2026-03-20 15:43:08 +0100

    Added templates

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 
-let loaded = 0;
-function start() {
-  if (loaded) return;
-  Kind.registerAddons({
-    'dtk_otp': import('./widgets/otp'),
-    'dtk_dialog': import('./widgets/dialog'),
-    'dtk_pwsetter': import('./widgets/pwsetter'),
-  })
-  loaded = 1;
-}
-
-
 /**
  * 
  */
-export function loadWidgets() {
-  if (document.readyState == 'complete') {
-    start()
-  } else {
-    if (location.hash) {
-      document.addEventListener('drumee:plugins:ready', start);
-    } else {
-      document.addEventListener('drumee:router:ready', start);
-    }
-  }
+export function loadSeeds() {
+  Kind.registerAddons(require('./seeds'))
 }
-
-export * from './utils/index';
-export * from './utils/validator';
-export * from './utils/contextmenu';
