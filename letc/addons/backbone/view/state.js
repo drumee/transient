3b033e94e6952cb3770a2ea33118b1604390abeb
Somanos Sar <somanos@drumee.com>
2026-04-30 11:20:31 +0700

    Changed view/state to sync state when called via setSet

--- letc/addons/backbone/view/state.js
+++ letc/addons/backbone/view/state.js
@@ -18,6 +18,7 @@ Backbone.View.prototype.setState = function(state, recursive) {
   if (_.isFunction(this._syncState)) {
     this._syncState(state);
   }
+  this.triggerMethod("also:click"); // Sync state to the chanel.
   if (recursive && (this.children != null ? this.children.length : undefined)) {
     return Array.from(this.children.toArray()).map((c) =>
       c.setState(state, recursive));
