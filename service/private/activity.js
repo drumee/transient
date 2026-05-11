889b32d9d620db9936f44222332fb23dbc910092
Vu Dang <vudangnt@gmail.com>
2026-05-11 14:51:16 +0700

    refactor(activity): clean up SQL query and ensure proper module export
    Removed an unused SQL query from the MfsActivity class and ensured the module export is correctly formatted. This change streamlines the code and improves maintainability.

--- service/private/activity.js
+++ service/private/activity.js
@@ -403,20 +403,4 @@ class MfsActivity extends Entity {
   }
 }
 
-module.exports = MfsActivity;    this.yp.await_query(
-        "SELECT a.id, a.timestamp AS ctime, a.uid AS author_id, " +
-        "       a.target_uid, a.event, a.data, " +
-        "       d.firstname    AS inviter_firstname, " +
-        "       d.lastname     AS inviter_lastname,  " +
-        "       d.email        AS inviter_email,     " +
-        "       e.headline     AS hub_headline,      " +
-        "       e.ident        AS hub_ident          " +
-        "  FROM yp.contact_activity a " +
-        "  LEFT JOIN yp.drumate d ON d.id = a.uid " +
-        "  LEFT JOIN yp.entity  e " +
-        "         ON e.id = JSON_UNQUOTE(JSON_EXTRACT(a.data, '$.hub_id')) " +
-        " WHERE a.target_uid = ? AND a.event = 'hub_invite_received' " +
-        "   AND a.dismissed_at IS NULL " +
-        " ORDER BY a.timestamp DESC LIMIT 50",
-        this.uid
-      ),
+module.exports = MfsActivity;
